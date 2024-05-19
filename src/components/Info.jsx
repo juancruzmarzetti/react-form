5bc3ab4f2d64e5a7c224ee76836286e4ad90ff7c
juancruzmarzetti <naujmarz@gmail.com>
2024-05-19 19:58:29 -0300

    plus-tailwind

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,9 +2,9 @@ const Info = ({user}) => {
   const {name, surname, favPkmn} = user;
   return (
     <>
-        <h3>Nombre: {name}</h3>
-        <h3>Apellido: {surname}</h3>
-        <h3>Pokemon favorito: {favPkmn}</h3>
+        <h3 className="text-3xl font-bold underline">Nombre: {name}</h3>
+        <h3 className="text-3xl font-bold underline">Apellido: {surname}</h3>
+        <h3 className="text-3xl font-bold underline">Pokemon favorito: {favPkmn}</h3>
     </>
   )
 }
